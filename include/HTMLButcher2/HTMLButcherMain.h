959d8a21f82ef9942a61057a31ce472fc6aba1d2
Rangel Reale <rangelspam@gmail.com>
2017-08-22 14:32:46 -0300

    * Add toolbars

--- include/HTMLButcher2/HTMLButcherMain.h
+++ include/HTMLButcher2/HTMLButcherMain.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QAction>
 
 namespace HTMLButcher2
 {
@@ -13,11 +14,13 @@ public:
 
 
 private:
+	void createActions();
+	void createToolBars();
+	void createMenus();
+public Q_SLOTS:
 	//
 	// MENUS
 	//
-	void createMenus();
-public Q_SLOTS:
 	void onMenuFileNew(bool checked = false);
 	void onMenuFileOpen(bool checked = false);
 	void onMenuFileClose(bool checked = false);
@@ -26,6 +29,19 @@ public Q_SLOTS:
 	void onMenuFileSaveForWeb(bool checked = false);
 	void onMenuFileSaveAllForWeb(bool checked = false);
 	void onMenuFilePreferences(bool checked = false);
+
+	void onMenuToolsInserHorLine(bool checked = false);
+	void onMenuToolsInserVerLine(bool checked = false);
+	void onMenuToolsMoveLine(bool checked = false);
+	void onMenuToolsDeleteLine(bool checked = false);
+	void onMenuToolsInsertAreaGlobal(bool checked = false);
+	void onMenuToolsDeleteAreaGlobal(bool checked = false);
+	void onMenuToolsInsertAreaMap(bool checked = false);
+	void onMenuToolsDeleteAreaMap(bool checked = false);
+	void onMenuToolsCancelOperation(bool checked = false);
+
+private:
+	QAction *filemenu_new, *filemenu_open, *filemenu_save;
 };
 
 } // namespace HTMLButcher2
--- src/HTMLButcherMain.cpp
+++ src/HTMLButcherMain.cpp
@@ -2,6 +2,8 @@
 
 #include <QMenuBar>
 #include <QMenu>
+#include <QToolBar>
+#include <QLabel>
 
 namespace HTMLButcher2
 {
@@ -11,7 +13,112 @@ HTMLButcherMain::HTMLButcherMain(QWidget *parent) :
 {
 	setWindowIcon(QIcon(":/hbico_48.png"));
 
+	createActions();
 	createMenus();
+	createToolBars();
+}
+
+void HTMLButcherMain::createActions()
+{
+	filemenu_new = new QAction(tr("&New"), this);
+	filemenu_new->setShortcut(QKeySequence::New);
+	filemenu_new->setIcon(QIcon(":/ico_new.png"));
+	filemenu_new->setIconVisibleInMenu(false);
+	connect(filemenu_new, &QAction::triggered, this, &HTMLButcherMain::onMenuFileNew);
+
+	filemenu_open = new QAction(tr("&Open"), this);
+	filemenu_open->setShortcut(QKeySequence::Open);
+	filemenu_open->setIcon(QIcon(":/ico_open.png"));
+	filemenu_open->setIconVisibleInMenu(false);
+	connect(filemenu_open, &QAction::triggered, this, &HTMLButcherMain::onMenuFileOpen);
+
+	filemenu_save = new QAction(tr("&Save"), this);
+	filemenu_save->setShortcut(QKeySequence::Save);
+	filemenu_save->setIcon(QIcon(":/ico_save.png"));
+	filemenu_save->setIconVisibleInMenu(false);
+	connect(filemenu_save, &QAction::triggered, this, &HTMLButcherMain::onMenuFileSave);
+}
+
+void HTMLButcherMain::createToolBars()
+{
+	//
+	// FILE
+	//
+	QToolBar *filetb = new QToolBar(tr("File"), this);
+	addToolBar(Qt::TopToolBarArea, filetb);
+
+	filetb->addAction(filemenu_new);
+	filetb->addAction(filemenu_open);
+	filetb->addAction(filemenu_save);
+
+	//
+	// VIEW
+	//
+	QToolBar *viewtb = new QToolBar(tr("View"), this);
+	addToolBar(Qt::TopToolBarArea, viewtb);
+
+	viewtb->addWidget(new QLabel(tr("View:"), this));
+
+	//
+	// TOOLS
+	//
+	QToolBar *toolstb = new QToolBar(tr("Tools"), this);
+	addToolBar(Qt::LeftToolBarArea, toolstb);
+
+	QAction *toolsmenu_inserthorline = new QAction(tr("Insert horizontal line"), this);
+	toolsmenu_inserthorline->setIcon(QIcon(":/ico_insert_hline.png"));
+	toolsmenu_inserthorline->setIconVisibleInMenu(false);
+	connect(toolsmenu_inserthorline, &QAction::triggered, this, &HTMLButcherMain::onMenuToolsInserHorLine);
+	toolstb->addAction(toolsmenu_inserthorline);
+
+	QAction *toolsmenu_insertverline = new QAction(tr("Insert vertical line"), this);
+	toolsmenu_insertverline->setIcon(QIcon(":/ico_insert_vline.png"));
+	toolsmenu_insertverline->setIconVisibleInMenu(false);
+	connect(toolsmenu_insertverline, &QAction::triggered, this, &HTMLButcherMain::onMenuToolsInserVerLine);
+	toolstb->addAction(toolsmenu_insertverline);
+
+	QAction *toolsmenu_moveline = new QAction(tr("Move line"), this);
+	toolsmenu_moveline->setIcon(QIcon(":/ico_move_line.png"));
+	toolsmenu_moveline->setIconVisibleInMenu(false);
+	connect(toolsmenu_moveline, &QAction::triggered, this, &HTMLButcherMain::onMenuToolsMoveLine);
+	toolstb->addAction(toolsmenu_moveline);
+
+	QAction *toolsmenu_deleteline = new QAction(tr("Delete line"), this);
+	toolsmenu_deleteline->setIcon(QIcon(":/ico_delete_line.png"));
+	toolsmenu_deleteline->setIconVisibleInMenu(false);
+	connect(toolsmenu_deleteline, &QAction::triggered, this, &HTMLButcherMain::onMenuToolsDeleteLine);
+	toolstb->addAction(toolsmenu_deleteline);
+
+	QAction *toolsmenu_insertgarea = new QAction(tr("Insert global area"), this);
+	toolsmenu_insertgarea->setIcon(QIcon(":/ico_insert_garea.png"));
+	toolsmenu_insertgarea->setIconVisibleInMenu(false);
+	connect(toolsmenu_insertgarea, &QAction::triggered, this, &HTMLButcherMain::onMenuToolsInsertAreaGlobal);
+	toolstb->addAction(toolsmenu_insertgarea);
+
+	QAction *toolsmenu_deletegarea = new QAction(tr("Delete global area"), this);
+	toolsmenu_deletegarea->setIcon(QIcon(":/ico_delete_garea.png"));
+	toolsmenu_deletegarea->setIconVisibleInMenu(false);
+	connect(toolsmenu_deletegarea, &QAction::triggered, this, &HTMLButcherMain::onMenuToolsDeleteAreaGlobal);
+	toolstb->addAction(toolsmenu_deletegarea);
+
+	QAction *toolsmenu_insertmarea = new QAction(tr("Insert map area"), this);
+	toolsmenu_insertmarea->setIcon(QIcon(":/ico_insert_marea.png"));
+	toolsmenu_insertmarea->setIconVisibleInMenu(false);
+	connect(toolsmenu_insertmarea, &QAction::triggered, this, &HTMLButcherMain::onMenuToolsInsertAreaMap);
+	toolstb->addAction(toolsmenu_insertmarea);
+
+	QAction *toolsmenu_deletemarea = new QAction(tr("Delete map area"), this);
+	toolsmenu_deletemarea->setIcon(QIcon(":/ico_delete_marea.png"));
+	toolsmenu_deletemarea->setIconVisibleInMenu(false);
+	connect(toolsmenu_deletemarea, &QAction::triggered, this, &HTMLButcherMain::onMenuToolsDeleteAreaMap);
+	toolstb->addAction(toolsmenu_deletemarea);
+
+	QAction *toolsmenu_cancelop = new QAction(tr("Cancel operation"), this);
+	toolsmenu_cancelop->setIcon(QIcon(":/ico_cancel.png"));
+	toolsmenu_cancelop->setIconVisibleInMenu(false);
+	connect(toolsmenu_cancelop, &QAction::triggered, this, &HTMLButcherMain::onMenuToolsCancelOperation);
+	toolstb->addAction(toolsmenu_cancelop);
+
 }
 
 /*********
@@ -25,14 +132,8 @@ void HTMLButcherMain::createMenus()
 	//
 	QMenu *filemenu = menuBar()->addMenu(tr("&File"));
 
-	QAction *filemenu_new = new QAction(tr("&New"), this);
-	filemenu_new->setShortcut(QKeySequence::New);
-	connect(filemenu_new, &QAction::triggered, this, &HTMLButcherMain::onMenuFileNew);
 	filemenu->addAction(filemenu_new);
 
-	QAction *filemenu_open = new QAction(tr("&Open"), this);
-	filemenu_open->setShortcut(QKeySequence::Open);
-	connect(filemenu_open, &QAction::triggered, this, &HTMLButcherMain::onMenuFileOpen);
 	filemenu->addAction(filemenu_open);
 
 	QAction *filemenu_close = new QAction(tr("C&lose"), this);
@@ -40,9 +141,6 @@ void HTMLButcherMain::createMenus()
 	connect(filemenu_close, &QAction::triggered, this, &HTMLButcherMain::onMenuFileClose);
 	filemenu->addAction(filemenu_close);
 
-	QAction *filemenu_save = new QAction(tr("&Save"), this);
-	filemenu_save->setShortcut(QKeySequence::Save);
-	connect(filemenu_save, &QAction::triggered, this, &HTMLButcherMain::onMenuFileSave);
 	filemenu->addAction(filemenu_save);
 
 	QAction *filemenu_saveas = new QAction(tr("Save &as..."), this);
@@ -75,6 +173,10 @@ void HTMLButcherMain::createMenus()
 	connect(filemenu_exit, &QAction::triggered, this, &HTMLButcherMain::close);
 	filemenu->addAction(filemenu_exit);
 }
+
+//
+// MENU: FILE
+//
 
 void HTMLButcherMain::onMenuFileNew(bool checked)
 {
@@ -112,6 +214,56 @@ void HTMLButcherMain::onMenuFileSaveAllForWeb(bool checked)
 }
 
 void HTMLButcherMain::onMenuFilePreferences(bool checked)
+{
+
+}
+
+//
+// MENU: TOOLS
+//
+
+void HTMLButcherMain::onMenuToolsInserHorLine(bool checked)
+{
+
+}
+
+void HTMLButcherMain::onMenuToolsInserVerLine(bool checked)
+{
+
+}
+
+void HTMLButcherMain::onMenuToolsMoveLine(bool checked)
+{
+
+}
+
+
+void HTMLButcherMain::onMenuToolsDeleteLine(bool checked)
+{
+
+}
+
+void HTMLButcherMain::onMenuToolsInsertAreaGlobal(bool checked)
+{
+
+}
+
+void HTMLButcherMain::onMenuToolsDeleteAreaGlobal(bool checked)
+{
+
+}
+
+void HTMLButcherMain::onMenuToolsInsertAreaMap(bool checked)
+{
+
+}
+
+void HTMLButcherMain::onMenuToolsDeleteAreaMap(bool checked)
+{
+
+}
+
+void HTMLButcherMain::onMenuToolsCancelOperation(bool checked)
 {
 
 }
