6b17a67c7a87c2e45a53e228babc720d5250df9d
Rangel Reale <rangelspam@gmail.com>
2017-08-22 15:12:13 -0300

    * Project

--- include/HTMLButcher2/Project/ButcherProject.h
+++ include/HTMLButcher2/Project/ButcherProject.h
@@ -11,10 +11,32 @@ class ButcherProject : public QObject
 public:
 	ButcherProject();
 
-	void New();
+	void newProject();
+	void close();
 
+	void open(const QString &filename);
+	void save(bool force = false);
+	void saveAs(const QString &filename, bool force = false);
+
+	bool isOpen();
+	bool isModified();
+
+	const QString &filename();
 Q_SIGNALS:
+	void onNew();
+	void onClosing();
+	void onClose();
+	void onOpen();
+	void onSave();
 
+	void onChanged();
+private:
+	void doLoad(const QString &filename);
+	void doSave(const QString &filename);
+
+	QString _filename;
+	bool _isopen;
+	bool _modified;
 };
 
 } } // HTMLButcher2::Project
--- src/Project/ButcherProject.cpp
+++ src/Project/ButcherProject.cpp
@@ -1,10 +1,100 @@
 #include "HTMLButcher2/Project/ButcherProject.h"
+#include "HTMLButcher2/Core/ButcherException.h"
 
 namespace HTMLButcher2 {
 namespace Project {
 
 ButcherProject::ButcherProject() :
-	QObject()
+	QObject(), _filename(), _isopen(false), _modified(false)
+{
+
+}
+
+void ButcherProject::newProject()
+{
+	close();
+
+	_filename = "";
+	_modified = true;
+
+	_isopen = true;
+
+	emit onNew();
+	emit onChanged();
+}
+
+void ButcherProject::close()
+{
+	if (_isopen)
+	{
+		emit onClosing();
+
+		_filename = "";
+		_modified = false;
+
+		_isopen = false;
+
+		emit onClose();
+		emit onChanged();
+	}
+}
+
+void ButcherProject::open(const QString &filename)
+{
+	close();
+
+	doLoad(filename);
+	_filename = filename;
+	_isopen = true;
+	_modified = true;
+
+	emit onOpen();
+	emit onChanged();
+}
+
+void ButcherProject::save(bool force)
+{
+	if (!_modified && !force)
+		return;
+
+	if (_filename.isEmpty())
+		throw Core::ButcherException(tr("File name not specified"));
+
+	doSave(_filename);
+
+	_modified = true;
+
+	emit onSave();
+	emit onChanged();
+}
+
+void ButcherProject::saveAs(const QString &filename, bool force)
+{
+	_filename = filename;
+	save(force);
+}
+
+bool ButcherProject::isOpen()
+{
+	return _isopen;
+}
+
+bool ButcherProject::isModified()
+{
+	return _modified;
+}
+
+const QString &ButcherProject::filename()
+{
+	return _filename;
+}
+
+void ButcherProject::doLoad(const QString &filename)
+{
+
+}
+
+void ButcherProject::doSave(const QString &filename)
 {
 
 }
